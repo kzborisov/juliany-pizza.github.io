1505afe466e4a9961657474d88728eb1f06d5d35
Kristian Borisov <kborisov@lely.com>
2021-02-05 16:08:03 +0200

    Optimizing changes.

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ const header = document.querySelector('.header.container');
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
-});	
+});
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
